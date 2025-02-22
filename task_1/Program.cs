d031f4e4d5f20a7e3da6f7eae8d6c91f5135c3e9
Yoya(Panda) <Yoya(Panda)@LAPTOP-UG5O1E3K>
2025-02-23 00:55:47 +0200

    Add project files.

---
+++ task_1/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("""
+                Welcome to Islam Carpet Cleaning Service!
+                    We charges:
+                    $25 per small carpet
+                    $35 per large carpet
+
+                    Note: Estimates are valid for 30 days
+                    Sales tax rate is 6% 
+
+                    Estimate for carpets cleaning service:
+                """);
+            Console.Write("Number of small carpets: ");
+            int smallOnes = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Number of large carpets: ");
+            int largeOnes = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"""
+                Price per small one: $25
+                Price per large one: $35
+                Cost : ${(smallOnes*25) + (largeOnes*35)}
+                Tax : ${((smallOnes*25) + (largeOnes*35)) * 0.06}
+                =======================================
+                Total estimates: ${((smallOnes * 25) + (largeOnes * 35)) * 1.06}
+                This estimate is valid for 30 days only
+                """);
+            
+        }
+    }
+}
